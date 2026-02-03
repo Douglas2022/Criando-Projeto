1d520b77ac229fc6dcc8616c13b291f38e7410fd
Doouglas Silva <paulodnunes2016@gmail.com>
2026-02-03 09:22:42 -0300

    Usando o Sort

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Couser.Device;
+
+namespace Couser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer() { SerialNumber = 1080 };
+            p.ProcessDoc("My letter");
+            p.Print("My letter");
+
+            Scanner S = new Scanner() { SerialNumber = 2003};
+            S.ProcessDoc("My Email");
+            Console.WriteLine(S.Scan());
+
+            ComboDevice c = new ComboDevice() { SerialNumber = 3921 };
+            c.ProcessDoc("My dissertatio");
+            c.Print("My dissertation");
+            Console.WriteLine(c.Scan());
+        }
+    }
+}
